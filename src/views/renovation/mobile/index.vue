465804f74538a7f3dc0f56f573e3db12e925f46d
wangkai <50765560@qq.com>
2022-04-17 23:41:39 +0800

    feat: 可视化路由

---
+++ src/views/renovation/mobile/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>移动端首页</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/renovation/pc/edit.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>pc端编辑</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
